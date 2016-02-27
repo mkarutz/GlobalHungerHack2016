da2f728ecdcf17cc4f4708353d49b2120c02115c
Malcolm Karutz <malcolm_karutz@live.com>
2016-02-28 05:16:15 +1100

    create a Feed

---
+++ src/DAOs/AddressDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkarutz
+ * Date: 28/02/16
+ * Time: 4:44 AM
+ */
+
+namespace FedUp\DAOs;
+
+
+class AddressDAO
+{
+
+}
---
+++ src/DAOs/RequestDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkarutz
+ * Date: 28/02/16
+ * Time: 4:45 AM
+ */
+
+namespace FedUp\DAOs;
+
+
+class RequestDAO
+{
+
+}
