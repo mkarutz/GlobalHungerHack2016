204de3a111db5041445d5889cbe0357a02fa5a8e
Malcolm Karutz <malcolm_karutz@live.com>
2016-02-28 01:55:23 +1100

    landing page

--- app/routes.php
+++ app/routes.php
@@ -6,13 +6,13 @@ use Silex\Application;
 
 $app->get('/', 'LandingController:dispatch');
 
-$app->get('/signup', 'SignUpController:get');
-$app->post('/signup', 'SignUpController:post');
+$app->get('/signup', 'UsersController:new');
+$app->post('/signup', 'UsersController:create');
 
-$app->get('/login', 'LoginController:get');
-$app->post('/login', 'LoginController:post');
+$app->get('/login', 'UsersController:login');
+$app->post('/login', 'UsersController:auth');
 
-$app->get('/logout', 'LogoutController:logout');
+$app->get('/logout', 'UsersController:logout');
 
 // Browse feeds
 $app->get('/feeds', 'FeedsController:index');
@@ -20,8 +20,24 @@ $app->get('/feeds', 'FeedsController:index');
 // View a feed
 $app->get('/feeds/{feedId}', 'FeedsController:view');
 
+// Send a request for an invitation
+$app->post('/feeds/{feedId}/requests/new', 'RequestsController:create');
+
+// View invitations
+$app->get('/invitations', 'InvitationsController:index');
+
+// View invitation and navigate to Feed
+$app->get('/invitations/{invitationId}', 'InvitationsController:view');
+
 // Create a feed
 $app->get('/feeds/new', 'FeedsController:new');
 $app->post('/feeds/new', 'FeedsController:create');
 
-$app->get('/feeds/new', 'FeedsController:new');
+// View requests
+$app->get('/requests', 'RequestsController:index');
+
+// View request details
+$app->get('/requests/{requestId}', 'RequestsController:view');
+
+// Accept the request: create a new invitation
+$app->post('/feeds/{feedId}/requests/{requestId}/accept', 'InvitationsController:send');
---
+++ src/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkarutz
+ * Date: 28/02/16
+ * Time: 1:30 AM
+ */
+
+namespace Controllers;
+
+
+class UserController
+{
+
+}
